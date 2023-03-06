988372ca68be6bd3a1f74e6d522455403c4707bc
emish8cool@gmail.com <emish8cool@gmail.com>
2023-03-06 18:41:12 +0000

    Completed hpo and train_model

--- train_model.py
+++ train_model.py
@@ -9,57 +9,158 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 
 import argparse
-
+import time
 
 #TODO: Import dependencies for Debugging andd Profiling
+from smdebug import modes
+from smdebug.profiler.utils import str2bool
+from smdebug.pytorch import get_hook
 
-def test(model, test_loader):
+def test(model, test_loader, criterion, hook):
     '''
     TODO: Complete this function that can take a model and a 
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
+          
     '''
-    pass
+    model.eval()
+    hook.set_mode(smd.modes.EVAL)
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += F.nll_loss(output, target, size_average=False).item()  # sum up batch loss
+            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-def train(model, train_loader, criterion, optimizer):
+    test_loss /= len(test_loader.dataset)
+    logger.info(
+        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
+
+
+def train(model, train_loader, validation_loader, criterion, optimizer,  epochs, hook):
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
     
+    c = 0
+    for e in range(epochs):
+        print('epoch',e)
+        
+        # training data
+        hook.set_mode(smd.modes.TRAIN)
+        model.train()
+        for (data, target) in train_loader:
+            optimizer.zero_grad()
+            outputs = model(data)
+            loss = criterion(outputs, target)
+            loss.backward()
+            optimizer.step()
+            c += len(data)
+        
+        # validaiton data
+        hook.set_mode(smd.modes.EVAL)
+        model.eval()
+        running_corrects = 0
+        with torch.no_grad():
+            for (data, target) in validation_loader:
+                outputs = model(data)
+                loss = criterion(outputs, target)
+                _, preds = torch.max(outputs, 1)
+                running_corrects += torch.sum(preds == target.data).item()
+        total_accuracy = running_corrects / len(validation_loader.dataset)
+        logger.info(f"Validation Set Avg Accuracy: {100*total_accuracy}%")
+    
+    return model    
+    
+
+# reference: Taken from course exercise
 def net():
     '''
     TODO: Complete this function that initializes your model
           Remember to use a pretrained model
     '''
-    pass
+    model = models.resnet18(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False   
+
+    num_features=model.fc.in_features
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 10))
+    return model
 
 def create_data_loaders(data, batch_size):
     '''
     This is an optional function that you may or may not need to implement
     depending on whether you need to use data loaders or not
     '''
-    pass
+    #train data loader
+    train_path = os.path.join(data, "train")
+    
+    train_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.Resize((224,224)),
+        transforms.ToTensor()
+    ])
+    
+    trainset = torchvision.datasets.ImageFolder(root=train_path, transform=train_transform)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    
+    #test data loader
+    test_path = os.path.join(data, "test")
+    
+    test_transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor()
+    ])
+    testset = torchvision.datasets.ImageFolder(root=test_path, transform=test_transform)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+    
+    #validation data loader
+    validation_path = os.path.join(data, "valid")
+    validationset = torchvision.datasets.ImageFolder(root=validation_path, transform=test_transform)
+    validation_loader = torch.utils.data.DataLoader(validationset, batch_size=batch_size, shuffle=False)
+   
+    return train_loader, validation_loader, test_loader
+
+
 
 def main(args):
     '''
     TODO: Initialize a model by calling the net function
     '''
+    #device
+    dev = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model=net()
+    model = model.to(dev)
     
     '''
     TODO: Create your loss and optimizer
     '''
-    loss_criterion = None
-    optimizer = None
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
+    
+     
+    # debugger hook
+    hook = smd.Hook.create_from_json_file()
+    hook.register_hook(model)
     
     '''
     TODO: Call the train function to start training your model
     Remember that you will need to set up a way to get training data from S3
     '''
-    model=train(model, train_loader, loss_criterion, optimizer)
+    
+    # calling loader 
+    train_loader, validation_loader, test_loader = create_data_loaders(data=args.data, batch_size=args.batch_size)
+    
+    model=train(model, train_loader, validation_loader, loss_criterion, optimizer, args.epochs, hook)
     
     '''
     TODO: Test the model to see its accuracy
@@ -70,13 +171,58 @@ def main(args):
     TODO: Save the trained model
     '''
     torch.save(model, path)
+       
 
 if __name__=='__main__':
     parser=argparse.ArgumentParser()
     '''
     TODO: Specify any training args that you might need
     '''
+    # ref: Course execeise Script Mode in SageMaker
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=14,
+        metavar="N",
+        help="number of epochs to train (default: 14)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)"
+    )
     
+     parser.add_argument(
+        "--data",
+        type=str,
+        default=os.environ["SM_CHANNEL_TRAIN"],
+        help="training data path in S3"
+    )
+    parser.add_argument(
+        "--model-dir",
+        type=str,
+        default=os.environ["SM_MODEL_DIR"],
+        help="location to save the model to"
+    )
     args=parser.parse_args()
+    
+    # logging information 
+    logging.info(f"Batch Size: {args.batch_size}")
+    logging.info(f"Epochs: {args.epochs}")
+    logging.info(f"Learning Rate: {args.lr}")
+    logging.info(f"Test Batch Size: {args.test_batch_size}")
+    
     
     main(args)
